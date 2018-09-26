0adff97af20f4866045968b54dee88ef19d2f98a
kevin115533 <kevin115533@yahoo.com>
2018-09-26 16:53:52 -0500

    populated board

--- ChessBoard.java
+++ ChessBoard.java
@@ -1,12 +1,18 @@
 public class ChessBoard{
 	private int[][] board = new int[8][8];
 	
+
 	public void printBoard(){
+		System.out.println("  A B C D E F G H");
 		for(int i = 0; i<8;i++){
+			System.out.print((8-i) + " ");
 			for(int j=0;j<8;j++){
-			System.out.println(board[i][j]);
+			//System.out.print(board[i][j] + " ");
+			System.out.print("  ");	
 			}
+			System.out.println("\n");
 		}
+		System.out.println("  A B C D E F G H");
 	}
 	
 	public void setSpot(int x, int y){
--- setupChessBoard.java
+++ setupChessBoard.java
@@ -4,6 +4,49 @@ public class setupChessBoard{
 		ChessBoard game = new ChessBoard();
 		
 		game.printBoard();
+		
+		
+		int count = 15;
+		char nonPawn[] = {'R','N','B','Q','K','B','N','R'};
+		String position[] ={"a1","b1","c1","d1","e1","f1","g1","h1"};
+	
+		
+		ChessPiece[] black = new ChessPiece[count];
+		
+		for(int i = 0;i<count; i++){
+			black[i] = new ChessPiece();
+			if(i<=7){
+				black[i].makePiece("b",nonPawn[i],position[i]);
+			}
+			else{			
+				black[i].makePiece("b",'P',"a2");
+			}
+		}
+		
+		ChessPiece[] white = new ChessPiece[count];
+		
+		for(int i = 0;i<count; i++){
+			white[i] = new ChessPiece();
+			if(i<=7){
+				white[i].makePiece("w",nonPawn[i],position[i]);
+			}
+			else{			
+				white[i].makePiece("w",'P',"a1");
+			}
+		}
+		
+		for(int i = 0;i<count; i++){
+			System.out.print(black[i].printInfo());
+			System.out.print(" ");
+		
+		}
+		System.out.print("\n");
+		for(int i = 0;i<count; i++){
+			System.out.print(white[i].printInfo());
+			System.out.print(" ");
+			
+		}
+		
 	}
 	
 	}
